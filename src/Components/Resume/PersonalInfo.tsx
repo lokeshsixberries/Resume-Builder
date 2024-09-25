813fc1b40948553d14ba9db156564bc3cafe878a
lokeshsixberries <lokesh@sixberries.com>
2024-09-25 15:33:57 +0530

    New Profile page fixed

--- src/Components/Resume/PersonalInfo.tsx
+++ src/Components/Resume/PersonalInfo.tsx
@@ -14,17 +14,23 @@ const PersonalInfo: React.FC = () => {
                 <Col xl="12">
                     <div>
                         <div className='employee-name text-left'>{personalInfoData.employeeName}</div>
-                        <div className='d-flex'>
-                            <div className='employee-profile text-left mx-1'>{personalInfoData.employeeRole}</div>
-                            <div className='resume-links text-left'>
+                        <div className='d-flex' style={{
+                            width: "100%"
+                        }}>
+                            <div className='resume-personal-profile-link text-left mx-1' style={{
+                                fontWeight: "700",
+                                color: "gray"
+                            }}>{personalInfoData.employeeRole}</div>
+                            <div className='resume-links text-left d-flex'>
                                 {personalInfoData.employeeEmail && <Link className='resume-personal-profile-link' style={{
-                                    color: "black"
+                                    color: "gray",
+                                    fontWeight: "700"
                                 }} href={personalInfoData.employeeEmail} target="_blank"><span className='resume-link-seprator'>|</span>{' '}+91 123231234{' '}</Link>}
-                                {personalInfoData.employeeEmail && <Link className='resume-personal-profile-link' href={personalInfoData.employeeEmail} target="_blank"><span className='resume-link-seprator'>|</span>{' '}{personalInfoData.employeeEmail}{' '}</Link>}
-                                {personalInfoData.employeeLinkedin && <Link className='resume-personal-profile-link' href={personalInfoData.employeeLinkedin} target="_blank"><span className='resume-link-seprator'>|</span>{' '}Linkedin{' '}</Link>}
-                                {personalInfoData.employeeGithub && <Link className='resume-personal-profile-link' href={personalInfoData.employeeGithub} target="_blank"><span className='resume-link-seprator'>|</span>{' '}Github{' '}</Link>}
-                                {personalInfoData.employeeTwitter && <Link className='resume-personal-profile-link' href={personalInfoData.employeeTwitter} target="_blank"><span className='resume-link-seprator'>|</span>{' '}Twitter{' '}</Link>}
-                                {personalInfoData.employeePortfolio && <Link className='resume-personal-profile-link' href={personalInfoData.employeePortfolio} target="_blank"><span className='resume-link-seprator'>|</span>{' '}Portfolio{' '}</Link>}
+                                {personalInfoData.employeeEmail && <Link className='resume-personal-profile-link' href={personalInfoData.employeeEmail} target="_blank"><span className='resume-link-seprator mx-1'>|</span>{' '}{personalInfoData.employeeEmail}{' '}</Link>}
+                                {personalInfoData.employeeLinkedin && <Link className='resume-personal-profile-link' href={personalInfoData.employeeLinkedin} target="_blank"><span className='resume-link-seprator mx-1'>|</span>{' '}Linkedin{' '}</Link>}
+                                {personalInfoData.employeeGithub && <Link className='resume-personal-profile-link' href={personalInfoData.employeeGithub} target="_blank"><span className='resume-link-seprator mx-1'>|</span>{' '}Github{' '}</Link>}
+                                {personalInfoData.employeeTwitter && <Link className='resume-personal-profile-link' href={personalInfoData.employeeTwitter} target="_blank"><span className='resume-link-seprator mx-1'>|</span>{' '}Twitter{' '}</Link>}
+                                {personalInfoData.employeePortfolio && <Link className='resume-personal-profile-link' href={personalInfoData.employeePortfolio} target="_blank"><span className='resume-link-seprator mx-1'>|</span>{' '}Portfolio{' '}</Link>}
                             </div>
                         </div>
                     </div>
